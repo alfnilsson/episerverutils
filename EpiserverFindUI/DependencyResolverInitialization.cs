80a6aeb316580341028cfa6e146df8b656b86196
mail@alfnilsson.se <mail@alfnilsson.se>
2016-05-05 19:50:28 +0200

    Changed registration to be transient

--- EpiserverFindUI/DependencyResolverInitialization.cs
+++ EpiserverFindUI/DependencyResolverInitialization.cs
@@ -6,7 +6,7 @@ using EPiServer.ServiceLocation;
 namespace Toders.Web.Find
 {
     [InitializableModule]
-    // Note that we're waiting until FindInitializationModule is done
+    // Note that we're waiting until FindInitializationModule is done.
     [ModuleDependency(typeof(FindInitializationModule))]
     public class DependencyResolverInitialization : IConfigurableModule
     {
@@ -16,8 +16,9 @@ namespace Toders.Web.Find
             var originalFindUIConfiguration = context.Container.GetInstance<IFindUIConfiguration>();
             context.Container.EjectAllInstancesOf<IFindUIConfiguration>();
             
-            // And giving it to my custom implementation as I want to reuse most of the original functionality
-            context.Container.Configure(x => x.For<IFindUIConfiguration>().Use(() => new WrappingFindUIConfiguration(originalFindUIConfiguration)));
+            // And giving it to my custom implementation as I want to reuse most of the original functionality.
+            // Here I'm registering it as Transient in the same way as the original was registered.
+            context.Services.AddTransient<IFindUIConfiguration, WrappingFindUIConfiguration>(locator => new WrappingFindUIConfiguration(originalFindUIConfiguration));
         }
 
         public void Initialize(InitializationEngine context)
